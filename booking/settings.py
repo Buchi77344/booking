d7de8c1f50502af59a0db005b8f7d25b316c18ad
Buchi77344 <onyebuchifestus77@gmail.com>
2024-09-17 12:00:33 +0100

    sljdjn

--- booking/settings.py
+++ booking/settings.py
@@ -132,18 +132,17 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'base.CustomUser'
 
 
-# settings.py
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST_USER = 'experiencehotspot@gmail.com'
+EMAIL_HOST_PASSWORD = 'brxsvnxviydhhpwz'  # Replace with actual password
+DEFAULT_FROM_EMAIL = 'experiencehotspot@gmail.com'  # Replace with a valid "from" address
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'experiencehotspot@gmail.com'
-EMAIL_HOST_PASSWORD = 'vrotokwjxuyzsjdo'  # Replace with actual password
-DEFAULT_FROM_EMAIL = 'experiencehotspot@gmail.com'  # Replace with a valid "from" address
 
-# # settings.py
-# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 
 
 
