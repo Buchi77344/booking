72a028d6dbcb71749690b9822d24ace6724f942b
AdejumoOlanrewaju <olanrewajuadejumo56@gmail.com>
2024-09-25 13:44:08 -0700

    commit

--- base/static/js/script.js
+++ base/static/js/script.js
@@ -180,9 +180,19 @@ if(document.querySelector(".calendar-container")){
  
 }
 
+
+let profileCta = document.querySelector(".profile-cta")
+console.log(profileCta)
+let profileDropDown = document.querySelector(".profile-drop-down")
+console.log(profileCta)
+if(document.querySelector(".profile-cta")){
+    profileCta.addEventListener("click", () =>{
+        profileDropDown.classList.toggle("visible")
+    })
+}
+
 if(document.querySelector(".love-icon")){
     const loveIcon = document.querySelectorAll(".love-icon-span");
-
 
     loveIcon.forEach(icon => {
         isLiked = false;
@@ -199,15 +209,6 @@ if(document.querySelector(".love-icon")){
         });
     })
 
-
-    let profileCta = document.querySelector(".profile-cta")
-    let profileDropDown = document.querySelector(".profile-drop-down")
-    console.log(profileCta)
-    if(document.querySelector(".profile-cta")){
-        profileCta.addEventListener("click", () =>{
-            profileDropDown.classList.toggle("visible")
-        })
-    }
 }
 
 //Animation on scroll
